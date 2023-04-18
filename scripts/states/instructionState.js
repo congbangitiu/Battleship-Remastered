0a31f5455de987eb7098f5473ed7be4dd4686936
Công Bằng <92241569+congbangitiu@users.noreply.github.com>
2023-04-18 22:42:53 +0700

    Complete Instruction state

--- scripts/states/instructionState.js
+++ scripts/states/instructionState.js
@@ -9,5 +9,19 @@ const instructionState = () => {
     text("Two players arrange five ships on their maps and then do guess-fire on each other's map in alternate turns until either player wins by sinking all the ships. Players get bonus turn if they hit opponents ship. BOT uses probability density map to guess coordinates of ship .\n\n 1. Press AUTO to arrange your ships randomly on map , press auto again if you want to re-arranage your ships in different order. \n\n 2. Press 'CONFIRM' to start the game. \n\n 3. NOTE : online-multiplayer is not yet developed. \n\n ENJOY GAME !!!!! ", 125, 125, 450,300);
 
     // Create button
-
-}
+    var backButton = new buttonClass("back", 225, 500);
+    backButton.draw();
+    //if the mouse is in the same place as the button
+    if (mouseX > backButton.x && mouseX < backButton.x + backButton.width && mouseY > backButton.y && mouseY < backButton.y + backButton.height) {
+        //check to see if the mouse is pressed
+        if (!mouseIsPressed) {
+            //if mouse is not pressed then light up button
+            backButton.lightUpButton();
+        }
+        else {
+            //if mouse is pressed go to menu
+            instructions = false;
+            menu = true;
+        }
+    }
+};
