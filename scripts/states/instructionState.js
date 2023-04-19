0bb10039f9013d4e8570253f1283d37a0931dd84
Nhat Huy <huyngnht1305@gmail.com>
2023-04-19 11:00:36 +0700

    add comment inn instructionState

--- scripts/states/instructionState.js
+++ scripts/states/instructionState.js
@@ -9,5 +9,5 @@ const instructionState = () => {
     text("Two players arrange five ships on their maps and then do guess-fire on each other's map in alternate turns until either player wins by sinking all the ships. Players get bonus turn if they hit opponents ship. BOT uses probability density map to guess coordinates of ship .\n\n 1. Press AUTO to arrange your ships randomly on map , press auto again if you want to re-arranage your ships in different order. \n\n 2. Press 'CONFIRM' to start the game. \n\n 3. NOTE : online-multiplayer is not yet developed. \n\n ENJOY GAME !!!!! ", 125, 125, 450,300);
 
     // Create button
-
+    /* This part has not done */
 }
