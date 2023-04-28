365bde692782538bcf5dda6637f1236cfdb08191
Nguyễn Hoàng Anh Tú <113959222+nghganhtu@users.noreply.github.com>
2023-04-28 22:20:40 +0700

    Reforming declare variables

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,7 +1,7 @@
-const player1AutoButton = new buttonClass("auto", 50, 400);
-const player2AutoButton = new buttonClass("auto", 550, 400);
-const player1ConfirmButton = new buttonClass("confirm", 250, 400);
-const player2ConfirmButton = new buttonClass("confirm", 750, 400);
+let player1AutoButton = new buttonClass("auto", 50, 400);
+let player2AutoButton = new buttonClass("auto", 550, 400);
+let player1ConfirmButton = new buttonClass("confirm", 250, 400);
+let player2ConfirmButton = new buttonClass("confirm", 750, 400);
 
-const posX = 400, posY = 70;
-const singlePlayerButton = new buttonClass("single-player", posX + 100, posY + 40);
+let posX = 400, posY = 70;
+let singlePlayerButton = new buttonClass("single-player", posX + 100, posY + 40);
