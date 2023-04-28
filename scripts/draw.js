dd22a87eed3baba05b85cc3e2dad870696303645
Nguyễn Hoàng Anh Tú <113959222+nghganhtu@users.noreply.github.com>
2023-04-28 22:27:08 +0700

    Inheritance method from buttonClass

--- scripts/draw.js
+++ scripts/draw.js
@@ -5,3 +5,10 @@ let player2ConfirmButton = new buttonClass("confirm", 750, 400);
 
 let posX = 400, posY = 70;
 let singlePlayerButton = new buttonClass("single-player", posX + 100, posY + 40);
+let multiPlayerButton = new buttonClass("multi-player", posX + 100, posY + 90);
+let onlineButton = new buttonClass("online", posX + 100, posY + 140);
+let instructionButton = new buttonClass("instructions", posX + 100, posY + 190);
+let creditButton = new buttonClass("credits", posX + 100, posY + 240);
+let statisticsButton = new buttonClass("statistics", posX + 100, posY + 290);
+
+let animate = new animation();
