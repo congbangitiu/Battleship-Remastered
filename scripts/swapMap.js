159904b7038e641218fcbea0004f1dc471564a1a
Nguyễn Hoàng Anh Tú <113959222+nghganhtu@users.noreply.github.com>
2023-04-29 23:21:14 +0700

    Done swapMap.js

--- scripts/swapMap.js
+++ scripts/swapMap.js
@@ -28,3 +28,45 @@ let createNewSinglePlayerObject = () => {
     bot = new botClass();
     bot.initializeGrid();
 };
+
+/**
+ * Takes argument as a string, if the parameter is "singlePlayer" it swaps the maps of player1 and bot,
+ *                              if the parameter is "multiPlayer" it swaps the maps of player1 and player2
+ *
+ * @Param {String} gameType  gameType = "singlePlayer" or "multiPlayer"
+ */
+
+let swapMap = (gameType) => {
+    let temp = new Array(10);
+    for (let i = 0; i < 10; i++) {
+        temp[i] = new Array(10);
+    }
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            temp[i][j] = player1.gridActual[i][j];
+        }
+    }
+
+    for (let  i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (gameType === "single-player") {
+                player1.gridActual[i][j] = bot.gridActual[i][j];
+            }
+            else {
+                player1.gridActual[i][j] = player2.gridActual[i][j];
+            }
+        }
+    }
+    
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (gameType === "single-player") {
+                bot.gridActual[i][j] = temp[i][j];
+            }
+            else {
+                player2.gridActual[i][j] = temp[i][j];
+            }
+        }
+    }
+};
