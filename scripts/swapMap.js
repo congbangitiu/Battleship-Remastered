14b9f945ce00d0ddba9fcf7031bf10cd21c3bde5
Nhat Huy <huyngnht1305@gmail.com>
2023-04-29 22:40:20 +0700

    Re-initialize instances of single player object Co-authored by: nghganhtu <nghganhtu@gmail.com>

--- scripts/swapMap.js
+++ scripts/swapMap.js
@@ -1,6 +1,7 @@
 // Create new instances of player1, player2
 let player1 = new playerClass("player-1", 1);
 let player2 = new playerClass("player-2", 2);
+let bot = new botClass();
 
 /**
  * Re-initialize instances of Multiplayer object
@@ -12,4 +13,18 @@ let createNewMultiPlayerObject = () => {
 
     player2 = new playerClass("player-2", 2);
     player2.initializeGrid();
+};
+
+/**
+ * Re-initialize instances of single player object
+ */
+
+let createNewSinglePlayerObject = () => {
+    player1 = new playerClass("player-1", 1);
+    player1.initializeGrid();
+
+    // Destructor equivalent for precious...
+    densityLens = false;
+    bot = new botClass();
+    bot.initializeGrid();
 };
