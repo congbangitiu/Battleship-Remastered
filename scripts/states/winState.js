f1c2725d3e8168c66a5211c6612e75ed76067cb5
Nguyen Quang Dieu <86088186+itzmealvin@users.noreply.github.com>
2023-05-11 00:11:43 +0700

    Modify logic statistics for Player 2

--- scripts/states/winState.js
+++ scripts/states/winState.js
@@ -191,88 +191,88 @@ let winStateCall = function () {
         statTableUpdated = true;
       }
     }
-//   } else if (player2.win === true) {
-//     textSize(40);
-//     text(
-//       "Player 2 VICTORY --- player 2 map !!! turns :- " + player2.turn,
-//       400,
-//       400,
-//       400,
-//       400
-//     );
+  } else if (player2.win === true) {
+    textSize(40);
+    text(
+      "Player 2 VICTORY --- player 2 map !!! turns :- " + player2.turn,
+      400,
+      400,
+      400,
+      400
+    );
 
-//     if (!statTableUpdated) {
-//       statTable[statisticsEnum.row.Player2][statisticsEnum.col.matchesWon]++;
-//       statTable[statisticsEnum.row.Player1][statisticsEnum.col.matchesLost]++;
+    if (!statTableUpdated) {
+      statTable[statisticsEnum.row.Player2][statisticsEnum.col.matchesWon]++;
+      statTable[statisticsEnum.row.Player1][statisticsEnum.col.matchesLost]++;
 
-//       // average turns to win
-//       statTable[statisticsEnum.row.Player2][statisticsEnum.col.avgTurnsToWin] +=
-//         player2.turn;
-//       statTable[statisticsEnum.row.Player2][statisticsEnum.col.avgTurnsToWin] =
-//         roundTo(
-//           statTable[statisticsEnum.row.Player2][
-//             statisticsEnum.col.avgTurnsToWin
-//           ] /
-//             statTable[statisticsEnum.row.Player2][statisticsEnum.col.matchesWon]
-//         );
+      // average turns to win
+      statTable[statisticsEnum.row.Player2][statisticsEnum.col.avgTurnsToWin] +=
+        player2.turn;
+      statTable[statisticsEnum.row.Player2][statisticsEnum.col.avgTurnsToWin] =
+        roundTo(
+          statTable[statisticsEnum.row.Player2][
+            statisticsEnum.col.avgTurnsToWin
+          ] /
+            statTable[statisticsEnum.row.Player2][statisticsEnum.col.matchesWon]
+        );
 
-//       // win percentage
-//       statTable[statisticsEnum.row.Player2][statisticsEnum.col.winPercentage] =
-//         roundTo(
-//           (statTable[statisticsEnum.row.Player2][
-//             statisticsEnum.col.matchesWon
-//           ] /
-//             (statTable[statisticsEnum.row.Player2][
-//               statisticsEnum.col.matchesWon
-//             ] +
-//               statTable[statisticsEnum.row.Player2][
-//                 statisticsEnum.col.matchesLost
-//               ])) *
-//             100.0
-//         );
-//       statTable[statisticsEnum.row.Player1][statisticsEnum.col.winPercentage] =
-//         roundTo(
-//           (statTable[statisticsEnum.row.Player1][
-//             statisticsEnum.col.matchesWon
-//           ] /
-//             (statTable[statisticsEnum.row.Player1][
-//               statisticsEnum.col.matchesWon
-//             ] +
-//               statTable[statisticsEnum.row.Player1][
-//                 statisticsEnum.col.matchesLost
-//               ])) *
-//             100.0
-//         );
-//       // number of ships destroyed
-//       let numberOfShipsDestroyed = 0;
-//       for (let i = 0; i < 5; i++) {
-//         if (player1.currLife[i] === 0) {
-//           numberOfShipsDestroyed++;
-//         }
-//       }
-//       let numberOfShipsLost = 0;
-//       for (let i = 0; i < 5; i++) {
-//         if (player2.currLife[i] === 0) {
-//           numberOfShipsLost++;
-//         }
-//       }
+      // win percentage
+      statTable[statisticsEnum.row.Player2][statisticsEnum.col.winPercentage] =
+        roundTo(
+          (statTable[statisticsEnum.row.Player2][
+            statisticsEnum.col.matchesWon
+          ] /
+            (statTable[statisticsEnum.row.Player2][
+              statisticsEnum.col.matchesWon
+            ] +
+              statTable[statisticsEnum.row.Player2][
+                statisticsEnum.col.matchesLost
+              ])) *
+            100.0
+        );
+      statTable[statisticsEnum.row.Player1][statisticsEnum.col.winPercentage] =
+        roundTo(
+          (statTable[statisticsEnum.row.Player1][
+            statisticsEnum.col.matchesWon
+          ] /
+            (statTable[statisticsEnum.row.Player1][
+              statisticsEnum.col.matchesWon
+            ] +
+              statTable[statisticsEnum.row.Player1][
+                statisticsEnum.col.matchesLost
+              ])) *
+            100.0
+        );
+      // number of ships destroyed
+      let numberOfShipsDestroyed = 0;
+      for (let i = 0; i < 5; i++) {
+        if (player1.currLife[i] === 0) {
+          numberOfShipsDestroyed++;
+        }
+      }
+      let numberOfShipsLost = 0;
+      for (let i = 0; i < 5; i++) {
+        if (player2.currLife[i] === 0) {
+          numberOfShipsLost++;
+        }
+      }
 
-//       statTable[statisticsEnum.row.Player2][
-//         statisticsEnum.col.shipsDestroyed
-//       ] += numberOfShipsLost;
-//       statTable[statisticsEnum.row.Player1][statisticsEnum.col.shipsLost] +=
-//         numberOfShipsLost;
-//       // number of ships lost
+      statTable[statisticsEnum.row.Player2][
+        statisticsEnum.col.shipsDestroyed
+      ] += numberOfShipsLost;
+      statTable[statisticsEnum.row.Player1][statisticsEnum.col.shipsLost] +=
+        numberOfShipsLost;
+      // number of ships lost
 
-//       statTable[statisticsEnum.row.Player2][statisticsEnum.col.shipsLost] +=
-//         numberOfShipsDestroyed;
-//       statTable[statisticsEnum.row.Player1][
-//         statisticsEnum.col.shipsDestroyed
-//       ] += numberOfShipsDestroyed;
+      statTable[statisticsEnum.row.Player2][statisticsEnum.col.shipsLost] +=
+        numberOfShipsDestroyed;
+      statTable[statisticsEnum.row.Player1][
+        statisticsEnum.col.shipsDestroyed
+      ] += numberOfShipsDestroyed;
 
-//       statTableUpdated = true;
-//     }
-//     player1.drawGridActual();
+      statTableUpdated = true;
+    }
+    player1.drawGridActual();
 //   } else {
 //     textSize(40);
 //     text("BOT Wins --- bot win !!! turns:-" + bot.turn, 400, 400, 400, 400);
